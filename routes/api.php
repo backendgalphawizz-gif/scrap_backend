141efb950f06b62f69daa14a21ba922046904114
husain <husain.aalphawizz@gmail.com>
2026-04-13 19:27:03 +0530

    update Balance

--- routes/api.php
+++ routes/api.php
@@ -105,6 +105,7 @@ Route::prefix('user')->middleware('auth:api')->group(function () {
     Route::get('voucher-brands', [UserVoucherController::class, 'brands']);
     Route::get('voucher-brands/{brandId}/vouchers', [UserVoucherController::class, 'byBrand']);
     Route::get('vouchers', [UserVoucherController::class, 'index']);
+    Route::get('vouchers/purchased', [UserVoucherController::class, 'purchasedVouchers']);
     Route::get('vouchers/purchase-transactions', [UserVoucherController::class, 'purchaseTransactions']);
     Route::post('vouchers/purchase', [UserVoucherController::class, 'purchase']);
     Route::get('vouchers/{id}', [UserVoucherController::class, 'show']);
